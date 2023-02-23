9d4c59217ebb1fd159be245fbea1d0b6714a3ed4
LDudrey <113869466+LDudrey@users.noreply.github.com>
2023-02-22 22:20:37 -0600

    fixed routes/api index import variable names

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./usersRoutes');
 const thoughtRoutes = require('./thoughtsRoutes');
 
-router.use('/users', usersRoutes);
-router.use('/thoughts', thoughtsRoutes);
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
