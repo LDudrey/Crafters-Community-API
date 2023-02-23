253d1a4b2250999daa39d2d06d1e1ca316c86d7f
LDudrey <113869466+LDudrey@users.noreply.github.com>
2023-02-22 21:14:09 -0600

    added thought.js model code

---
+++ models/Thought.js
@@ -0,0 +1,44 @@
+const { Schema, model } = require('mongoose');
+const userSchema = require('./Thought');
+const reactionSchema = require('./Reaction');
+
+const userSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            min_length: 1,
+            max_length: 280,
+        },
+        // https://stackoverflow.com/questions/52867578/mongoose-date-schema
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+            get: date => date.toDateString(),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'reaction',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
